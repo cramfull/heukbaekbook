5403ba79590d6a41373b0bb15dab7a824c57e734
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2024-10-23 15:53:58 +0900

    first commit

---
+++ src/main/java/com/nhnacademy/heukbaekeureka/HeukbaekEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeukbaekEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeukbaekEurekaApplication.class, args);
+    }
+
+}
