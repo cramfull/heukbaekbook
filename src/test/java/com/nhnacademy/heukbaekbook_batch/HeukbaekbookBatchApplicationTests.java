52b33c56d654e11f27da71d831501e25cba0567b
cramfull <tmdgud99@gmail.com>
2024-11-29 20:11:47 +0900

    [Fix] HotFix

--- src/test/java/com/nhnacademy/heukbaekbook_batch/HeukbaekbookBatchApplicationTests.java
+++ src/test/java/com/nhnacademy/heukbaekbook_batch/HeukbaekbookBatchApplicationTests.java
@@ -3,7 +3,7 @@ package com.nhnacademy.heukbaekbook_batch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class HeukbaekbookBatchApplicationTests {
 
 	@Test
